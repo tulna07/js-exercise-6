f0f76179033f08d26b76e7e3e7dd18973391d970
scoobytux <tu.lna07@gmail.com>
2022-03-22 15:52:22 +0700

    Add open questions functionality

---
+++ assets/js/modules/dom.js
@@ -0,0 +1,19 @@
+/**
+ * General DOM ---------------------------------
+ */
+export const body = document.querySelector(".body");
+export const toggleThemeBtn = document.querySelector(".btn-toggle-theme");
+export const headerLogo = document.querySelector(".header__logo");
+
+export const questionOpenBtns = document.querySelectorAll(
+  ".question-list .btn-open-question"
+);
+export const questionBoxes = document.querySelectorAll(".question-box");
+
+/**
+ * Loop questions DOM ---------------------------------
+ */
+
+/**
+ * Array questions DOM ---------------------------------
+ */
